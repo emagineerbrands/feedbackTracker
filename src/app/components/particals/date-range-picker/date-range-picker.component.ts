7d4e96d26158f9d19093fe58e4e128824e98250a
Jayakumar-Kautharam <munijaykumar.kauthram@sails.solutions>
2024-08-06 16:38:35 +0530

    UTC Date Update

--- src/app/components/particals/date-range-picker/date-range-picker.component.ts
+++ src/app/components/particals/date-range-picker/date-range-picker.component.ts
@@ -1,19 +1,44 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, Input, Output, OnInit, AfterViewInit, ChangeDetectorRef   } from '@angular/core';
+import { NgbDateStruct , NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-date-range-picker',
   templateUrl: './date-range-picker.component.html',
   styleUrl: './date-range-picker.component.css'
 })
-export class DateRangePickerComponent {
+export class DateRangePickerComponent implements OnInit{
 
   @Input() fromDate!: NgbDateStruct | null;
   @Input() toDate!: NgbDateStruct | null;
 
   @Output() DateRangePicker = new EventEmitter<any>();
 
-  constructor() {}
+  constructor(private config: NgbDatepickerConfig, private cdr: ChangeDetectorRef) {
+    //this.dateConfig();
+  }
+
+  ngOnInit() {
+    this.dateConfig();
+  }
+
+  ngAfterViewInit(){
+    //this.dateConfig();
+  }
+
+  dateConfig(){
+    const today = new Date();
+    const utcDate = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate()));
+
+    const defaultDate: NgbDateStruct = {
+      year: utcDate.getUTCFullYear(),
+      month: utcDate.getUTCMonth() + 1, // NgbDateStruct months are 1-based
+      day: utcDate.getUTCDate(),
+    };
+    // Set the default view date in the datepicker without populating the input fields
+    this.config.startDate = defaultDate;
+    //this.cdr.detectChanges();
+
+  }
 
   onDateRangeChange() {
     if (this.fromDate && this.toDate) {
